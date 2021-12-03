da0a3942ba0b362fdd2876f6808ac9678103c8aa
ahmadsadek12 <ahmadsadek8@hotmail.com>
2021-12-03 22:20:38 +0200

    setting code layout

--- src/main/java/com/LinkIT/LocalRepositoryController.java
+++ src/main/java/com/LinkIT/LocalRepositoryController.java
@@ -1,14 +1,38 @@
 package com.LinkIT;
 
 public class LocalRepositoryController {
-    public void pullRepository(){
+    private static String REPOSITORIES_BASE_DIR = "./repositories";
+    private String repositoryName;
+    private String repositoryRemoteUrl;
 
+    public LocalRepositoryController(String repositoryName, String repositoryRemoteUrl) {
+        this.repositoryName = repositoryName;
+        this.repositoryRemoteUrl = repositoryRemoteUrl;
+    }
+
+    private void checkoutBranch() {
+
+    }
+
+    public void pullRepository(){
+        // Make sure that the folder to pull the repository into exists
+
+        // Pull the code into the folder
     }
     public void pushRepository(){
-
+        // Push the repository into github
     }
-    public String[] getMergeConflictFiles(){
 
+    public String[] getMergeConflictFiles(String branchName){
+        // Checkout the branch
+
+        // Try to merge it with master
+
+        // Check if the merge succeeded
+
+        // If success, return an empty list
+
+        // If merging failed, get the list of files that have conflicts
         return new String[0];
     }
 }
