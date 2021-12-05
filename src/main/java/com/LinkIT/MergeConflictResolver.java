e6744a7d1767d658a54b60fa6fada0e4c1b0e538
Mira Mansour <81388088+m-mansour1@users.noreply.github.com>
2021-12-05 23:46:47 +0200

    added to file

--- src/main/java/com/LinkIT/MergeConflictResolver.java
+++ src/main/java/com/LinkIT/MergeConflictResolver.java
@@ -18,27 +18,27 @@
         ProcessBuilder processBuilder = new ProcessBuilder();
         boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
 
-        public void package_lock_conflict_resolver(){
+        public void package_lock_conflict_resolver() throws IOException {
             if(isWindows){
-                processBuilder.command("cmd.exe", "/c", "rm package-lock.json");
-                processBuilder.command("cmd.exe", "/c", "npm i"); //do these commands which fix package-lock conflict
+                processBuilder.command("cmd.exe", "/c", "rm package-lock.json").start();
+                processBuilder.command("cmd.exe", "/c", "npm i").start(); //do these commands which fix package-lock conflict
             }else{
-                processBuilder.command("sh", "-c", "rm package-lock.json");
-                processBuilder.command("sh", "-c", "npm i"); //do these commands which fix package-lock conflict
+                processBuilder.command("sh", "-c", "rm package-lock.json").start();
+                processBuilder.command("sh", "-c", "npm i").start(); //do these commands which fix package-lock conflict
             }
         }
 
         public void automerge(GHPullRequest pullRequest) throws IOException {
             if(isWindows){
-                processBuilder.command("cmd.exe", "/c", "git add ."); 
-                processBuilder.command("cmd.exe", "/c", "git commit -m 'bot merging branch to master'"); 
-                processBuilder.command("cmd.exe", "/c", "git checkout master"); 
-                processBuilder.command("cmd.exe", "/c", "git merge new-branch"); //find and add branch
+                processBuilder.command("cmd.exe", "/c", "git add .").start();
+                processBuilder.command("cmd.exe", "/c", "git commit -m 'bot merging branch to master'").start();
+                processBuilder.command("cmd.exe", "/c", "git checkout master").start();
+                processBuilder.command("cmd.exe", "/c", "git merge new-branch").start(); //find and add branch
             }else{
-                processBuilder.command("sh", "-c", "git add .");
-                processBuilder.command("sh", "-c", "git commit -m 'bot merging branch to master'");
-                processBuilder.command("sh", "-c", "git checkout master");
-                processBuilder.command("sh", "-c", "git merge new-branch"); //find and add branch
+                processBuilder.command("sh", "-c", "git add .").start();
+                processBuilder.command("sh", "-c", "git commit -m 'bot merging branch to master'").start();
+                processBuilder.command("sh", "-c", "git checkout master").start();
+                processBuilder.command("sh", "-c", "git merge new-branch").start(); //find and add branch
             }
             
             if (checkTags.checkStopMergeTag(pullRequest)){
