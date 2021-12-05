34e4d735d0014c5efee590dcc98740ef681b3a7a
Mira Mansour <81388088+m-mansour1@users.noreply.github.com>
2021-12-05 23:26:08 +0200

    changed in mergewith master

--- src/main/java/com/LinkIT/MergeMasterWithCurrentBranch.java
+++ src/main/java/com/LinkIT/MergeMasterWithCurrentBranch.java
@@ -10,19 +10,10 @@ import java.util.Optional;
 
 public class MergeMasterWithCurrentBranch {
     private GHBranch ghBranch;
-    private GHBranchProtection ghBranchProtection;
-    private GHCommit ghCommit;
     private GHRepository current;
     private int branch_merge_counter = 0;
 
     void MergeMasterWithCurrentBranch() throws IOException {
-
-        //$ git add –A
-        //$ git commit –m "Some commit message"
-        //$ git checkout master
-        //Switched to branch 'master'
-        //$ git merge new-branch
-
         ghBranch.disableProtection();
         ghBranch.merge(getMasterBranch(), "Merging Master Branch with the current Branch");
         branch_merge_counter++;
@@ -31,29 +22,6 @@ public class MergeMasterWithCurrentBranch {
     public int getBranchMergeCounter(){
         return branch_merge_counter;
     }
-
-//    public String resolveRemoteBranch() {
-//        // Let's try use repository branch from .updatebot.yml first
-//        GitRepositoryConfig config = repo.getRepositoryDetails();
-//        if (config == null) {
-//            LOG.warn("The repo has no config!");
-//            return "master";
-//        }
-//        if(!Strings.empty(config.getBranch())) {
-//            return config.getBranch();
-//        } // Try detect Github repository and use its default branch
-//        else if(repo instanceof GithubRepository) {
-//            GHRepository ghRepository = GitHubHelpers.getGitHubRepository(this);
-//            if (ghRepository != null) {
-//                config.setBranch(ghRepository.getDefaultBranch());
-//            }
-//        }
-//        // Fallback to master branch for Git repositories
-//        if(Strings.empty(config.getBranch())) {
-//            config.setBranch("master");
-//        }
-//        return config.getBranch();
-//    }
 
     private GHBranch getMasterBranch() throws IOException {
         return current.getBranch("master");
